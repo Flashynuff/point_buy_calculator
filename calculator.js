65460f6dfcac364f2a47439bd2c6f039ae95d2a8
Flashynuff <Flashynuff@gmail.com>
2017-10-08 19:11:45 -0400

    More work on the calculations

--- calculator.js
+++ calculator.js
@@ -1,11 +1,12 @@
-function AbilityRow(name, baseScore, pointCosts) {
+function AbilityRow(name, baseScore, pointCosts, pointsOffset) {
 	var self = this;
 	self.name = name;
-	self.offset = ko.observable(0)
+	self.scoreOffset = ko.observable(0)
 	self.score = ko.computed(function() {
-		return parseInt(baseScore()) + parseInt(self.offset());
+		return parseInt(baseScore()) + parseInt(self.scoreOffset());
 	}, this);
-	self.pointCost = ko.computed(function() {
+
+	self.totalPointCost = ko.computed(function() {
 		return pointCosts()[self.score() - 1].cost();
 	}, this);
 
@@ -14,13 +15,19 @@ function AbilityRow(name, baseScore, pointCosts) {
 	}, this);
 
 	self.increase = function() {
-		var prev = self.offset();
-		self.offset(prev + 1);
+		var prevScoreOffset = self.scoreOffset();
+		self.scoreOffset(prevScoreOffset + 1);
+		var prevPointsUsed = pointsOffset();
+		var cost = Math.abs(pointCosts()[self.score() - 1].cost());
+		pointsOffset(prevPointsUsed + cost);
 	};
 
 	self.decrease = function() {
-		var prev = self.offset();
-		self.offset(prev - 1);
+		var prevScoreOffset = self.scoreOffset();
+		self.scoreOffset(prevScoreOffset - 1);
+		var prevPointsUsed = pointsOffset();
+		var cost = Math.abs(pointCosts()[self.score() - 1].cost());
+		pointsOffset(prevPointsUsed - cost);
 	};
 }
 
@@ -34,8 +41,11 @@ function CalculatorViewModel() {
 	var self = this;
 	self.baseScore = ko.observable(config.baseScore);
 	self.pointTotal = ko.observable(config.pointTotal);
-
 	self.scoreCap = ko.observable(config.scoreCap);
+	self.pointsOffset = ko.observable(0);
+	self.pointPool = ko.computed(function() {
+		return parseInt(self.pointTotal()) - parseInt(self.pointsOffset())
+	})
 
 	self.pointCosts = ko.observableArray();
 	for (var i = 0; i < config.pointCosts.length; i++){
@@ -44,12 +54,12 @@ function CalculatorViewModel() {
 		);
 	}
 
-	console.log(self.pointCosts);
-
 	self.abilityRows = ko.observableArray();
 	for(var i = 0; i < config.abilityNames.length; i++){
-		self.abilityRows.push(new AbilityRow(config.abilityNames[i], self.baseScore, self.pointCosts))
+		self.abilityRows.push(new AbilityRow(config.abilityNames[i], self.baseScore, self.pointCosts, self.pointsOffset))
 	}
+
+	//TODO: Add reset all
 	
 }
 

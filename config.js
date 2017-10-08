6e84ff6461be0a42ebe664121e3dc4266b04db23
Flashynuff <Flashynuff@gmail.com>
2017-10-08 12:49:50 -0400

    add config file

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = new Object();
+config.baseScore = 6;
+config.pointTotal = 31;
+config.scoreCap = 18;
+config.pointCosts = [-1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 4];
+config.abilityModifiers = [-5, -4, -4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10];
